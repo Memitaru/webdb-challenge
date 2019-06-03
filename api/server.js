6cd561f324924477915163c377b587add121f0c9
Memitaru <amandatwah@gmail.com>
2019-06-02 18:35:23 -0700

    Inital API setup

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Welcome to the API"}.message)
+})
+
+
+module.exports = server;
